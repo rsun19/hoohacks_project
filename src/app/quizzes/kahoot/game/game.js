88336425bbdd6ea442f207c8eccca8098f7cb294
rsun19 <robertssun1234@gmail.com>
2024-03-24 01:37:53 -0400

    added sockets

--- src/app/quizzes/kahoot/game/game.js
+++ src/app/quizzes/kahoot/game/game.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import { useSearchParams, redirect } from 'next/navigation'
 import './game.css'
+import io from 'socket.io-client'
 
 const questions = [
   {
@@ -27,6 +28,8 @@ const questions = [
 const maxPoints = 1000
 const questionTimer = 10
 
+const socket = io('http://localhost:3030')
+
 const Game = () => {
   const searchParams = useSearchParams()
   const initialQuestionIndex = parseInt(searchParams.get('next') || '0', 10)
@@ -35,6 +38,8 @@ const Game = () => {
   const [playerScores, setPlayerScores] = useState({})
   const [showScore, setShowScore] = useState(false)
 
+  // incomplete websockets for now
+  const startTime = Date.now()
   useEffect(() => {
     if (timer === 0) {
       redirect(`/kahoot/game/scoreboard?next=${currentQuestion + 1}`)
@@ -67,10 +72,23 @@ const Game = () => {
         ...prevScores,
         playerName: (prevScores.playerName || 0) + points
       }))
+
+      console.log(JSON.stringify([currentQuestion.toString(), 1, 'user 1']).toString())
+      socket.on('connect', function () {
+        const data = [currentQuestion.toString(), 1, 'user 1']
+        socket.emit('curAnswer', JSON.stringify(data).toString())
+        console.log(currentQuestion)
+      })
+      redirect('/kahoot/game/scoreboard')
     }
 
     const nextQuestion = currentQuestion + 1
     if (nextQuestion < questions.length) {
+      socket.on('connect', function () {
+        const data = [currentQuestion.toString(), 0, 'user 1']
+        socket.emit('curAnswer', JSON.stringify(data).toString())
+        console.log(currentQuestion)
+      })
       redirect('/kahoot/game/scoreboard')
       setShowScore(true)
     }
